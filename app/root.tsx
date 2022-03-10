9c3120950b68ced30c13d0cbcfd82137786152fb
vanderms <vanderms.84@gmail.com>
2022-03-10 17:51:12 -0300

    setup project

--- app/root.tsx
+++ app/root.tsx
@@ -7,9 +7,27 @@ import {
   ScrollRestoration,
 } from "remix";
 import type { MetaFunction } from "remix";
+import type { LinksFunction } from "remix";
+import indexCSS from './styles/index.css';
 
 export const meta: MetaFunction = () => {
-  return { title: "New Remix App" };
+  return { title: "Frontend Mentor | Art Gallery Website" };
+};
+
+export const links: LinksFunction = () => {
+  return [
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '32x32',
+      href: '/assets/favicon-32x32.png',
+    },
+    {
+      rel: 'stylesheet',
+      href: indexCSS,
+      'data-map': ''
+    },
+  ];
 };
 
 export default function App() {
