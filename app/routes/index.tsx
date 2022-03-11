dbb75b122476e93e28dbdd7c0b4e7b2bd9942942
vanderms <vanderms.84@gmail.com>
2022-03-11 18:55:09 -0300

    implement footer mobile design

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,11 +2,15 @@ import Footer from "~/components/sections/footer/footer";
 import SectionGallery from "~/components/sections/gallery/gallery";
 import SectionHero from "~/components/sections/hero/hero";
 
+/*
+ <SectionHero/>
+      <SectionGallery/>
+*/
+
 export default function Index() {
   return (
     <main>
-      <SectionHero/>
-      <SectionGallery/>
+     
       <Footer/>
     </main>
   );
