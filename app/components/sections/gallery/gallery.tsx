93ad86333c0ed8e5ab4739e7bd1b9711895c1916
vanderms <vanderms.84@gmail.com>
2022-03-11 13:40:39 -0300

    implement section gallery mobile design

--- app/components/sections/gallery/gallery.tsx
+++ app/components/sections/gallery/gallery.tsx
@@ -1,16 +1,30 @@
-import { Media } from "~/lib/media";
+import ResponsivePicture from '~/components/ui/picture/responsive-picture';
 
 export default function SectionGallery() {
   return (
-    <section className="rt-section-gallery">
+    <section
+      aria-labelledby="section-gallery-h2-title"
+      className="rt-section-gallery"
+    >
       <header className="content">
-        <h2 className="h2-title">Your Day at the Gallery</h2>
+        <h2 id="section-gallery-h2-title" className="h2-title">
+          Your Day at the Gallery
+        </h2>
         <p className="h2-description">
           Wander through our distinct collections and find new insights about
           our artists. Dive into the details of their creative process.
         </p>
       </header>
-      
+      <ResponsivePicture name="image-grid-1" />
+      <ResponsivePicture name="image-grid-2" />
+      <ResponsivePicture name="image-grid-3" />
+      <article className="inspired">
+        <h3 className="h3-title">COME & BE INSPIRED</h3>
+        <p className="h3-description">
+          We’re excited to welcome you to our gallery and see how our
+          collections influence you.
+        </p>
+      </article>
     </section>
   );
 }
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,9 +1,11 @@
+import SectionGallery from "~/components/sections/gallery/gallery";
 import SectionHero from "~/components/sections/hero/hero";
 
 export default function Index() {
   return (
     <main>
       <SectionHero/>
+      <SectionGallery/>
     </main>
   );
 }
