51bb392de5d71a4b507ed96cf7aa36c55c4dbda4
vanderms <vanderms.84@gmail.com>
2022-03-12 06:39:27 -0300

    add meta tags

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,6 +1,21 @@
 import Footer from '~/components/sections/footer/footer';
 import SectionGallery from '~/components/sections/gallery/gallery';
 import SectionHero from '~/components/sections/hero/hero';
+import type { MetaFunction } from 'remix';
+
+export const meta: MetaFunction = () => {
+  return {
+    'og:site_name': 'Frontend Mentor | Art Gallery Website',
+    'og:title': 'Art Gallery Website',
+    'og:description':
+      'My solution to the challenge Art Gallery Website, by Frontend Mentor',
+    'og:image':
+      'https://art-gallery-website-fe370.pages.dev/assets/preview.jpg',
+    'og:type': 'website',
+    'og:image:type': 'image/jpeg',
+    'og:url': 'https://art-gallery-website-fe370.pages.dev/',
+  };
+};
 
 export default function Index() {
   return (
