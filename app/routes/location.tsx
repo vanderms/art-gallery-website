f51e3e6b2192b01e8454b1654c98d7a2c723a72e
vanderms <vanderms.84@gmail.com>
2022-03-12 06:26:19 -0300

    implement project responsive design

--- app/routes/location.tsx
+++ app/routes/location.tsx
@@ -1,11 +1,13 @@
 import SectionMap from '~/components/sections/map/map';
 import SectionLocation from '~/components/sections/location/location';
+import Footer from '~/components/sections/footer/footer';
 
 export default function Location() {
   return (
-    <main>
+    <main className="rt-page-location">
       <SectionMap />
       <SectionLocation />
+      <Footer/>
     </main>
   );
 }
